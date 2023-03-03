593ceb1fc7961aaddf76743387757ac1d4981bef
jugla <jugla@free.fr>
2023-03-03 21:12:16 +0100

    manage python 3.11

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyKeyAtome",
-    version="2.1.1",
+    version="2.1.2",
     license="MIT",
     author="jugla",
     author_email="jugla@users.github.com",
